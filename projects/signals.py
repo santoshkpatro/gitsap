e7f5875341c78bb225e542d1a311993823d72a3a
Santosh Kumar Patro <santoshkpatro11@gmail.com>
2025-05-08 20:15:36 +0530

    feat: Update project signal to initialize cloud resource artifact on creation

--- projects/signals.py
+++ projects/signals.py
@@ -7,7 +7,7 @@ from projects.models import Project, ProjectCollaborator
 @receiver(post_save, sender=Project)
 def initialize_git_repo_and_store(sender, instance, created, **kwargs):
     if created and not instance.resource:
-        instance.init_repo()
+        instance._setup_cloud_resource_artifact()
 
 
 @receiver(post_save, sender=Project)
