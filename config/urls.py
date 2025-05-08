369c839571c3ed07187252570f715783446394c3
Santosh Kumar Patro <hey@santoshkpatro.in>
2025-05-08 20:54:54 +0530

    feat: Add project overview view, template, and update URL routing

--- config/urls.py
+++ config/urls.py
@@ -3,11 +3,13 @@ from django.urls import path
 
 from home.views import IndexView
 from accounts.views import LoginView, RegisterView
+from projects.views import ProjectOverview
 
-
+# fmt: off
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/login/", LoginView.as_view(), name="accounts-login"),
     path("accounts/register/", RegisterView.as_view(), name="accounts-register"),
+    path("<str:username>/<str:project_handle>/", ProjectOverview.as_view(), name="project-overview"),
     path("", IndexView.as_view(), name="home-index"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+
+class ProjectOverview(View):
+    def get(self, request, *args, **kwargs):
+        context = {}
+        return render(request, "projects/overview.html", context)
