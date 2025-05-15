d4939c386ee8c6d0bae0c9fad36cb46ff58e746b
Santosh Kumar Patro <hey@santoshkpatro.in>
2025-05-15 22:49:12 +0530

    feat: Add PullRequestCreateForm and corresponding view for pull request creation

--- pull_requests/views.py
+++ pull_requests/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+from projects.mixins import ProjectAccessMixin
+
+
+class PullRequestCreateView(ProjectAccessMixin, View):
+    def get(self, request, *args, **kwargs):
+        # Render the pull request creation form
+        return render(request, "pull_requests/create.html", {})
