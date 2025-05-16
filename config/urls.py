5cf998e1e1e96ecece18403f21b5de4a0745c7cb
Santosh Kumar Patro <hey@santoshkpatro.in>
2025-05-17 03:18:15 +0530

    refactor: streamline urlpatterns for project and issue handling

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import path, include, re_path
-from django.http import HttpResponse
 
 from home.views import IndexView
 from accounts.views import LoginView, RegisterView, LogoutView
@@ -26,22 +25,26 @@ from pull_requests.views import (
     PullRequestCreateView,
 )
 
+# fmt: off
 project_browse_urlpatterns = [
-    re_path(
-        r"^tree/(?P<ref_and_path>.+)$",  # Capture entire string after /tree/
-        ProjectTreeView.as_view(),
-        name="project-tree",
-    ),
-    re_path(
-        r"^blob/(?P<ref_and_path>.+)$",  # Capture entire string after /blob/
-        ProjectBlobView.as_view(),
-        name="project-blob",
-    ),
-    re_path(
-        r"^commits/(?P<ref>.+)$",  # Capture entire string after /commits/
-        ProjectCommitHistoryView.as_view(),
-        name="project-commit-history",
-    ),
+    re_path(r"^tree/(?P<ref_and_path>.+)$",  ProjectTreeView.as_view(), name="project-tree"),
+    re_path(r"^blob/(?P<ref_and_path>.+)$",  ProjectBlobView.as_view(), name="project-blob"),
+    re_path(r"^commits/(?P<ref>.+)$",  ProjectCommitHistoryView.as_view(), name="project-commit-history"),
+]
+
+# fmt: off
+project_urlpatterns = [
+    path("", ProjectOverview.as_view(), name="project-overview"),
+    path("browse/", include(project_browse_urlpatterns)),
+    path("issues/", IssueListView.as_view(), name="issue-list"),
+    path("issues/create/", IssueCreateView.as_view(), name="issue-create"),
+    path("issues/<int:issue_number>/", IssueDetailView.as_view(), name="issue-detail"),
+    path("issues/<int:issue_number>/close/", IssueCloseView.as_view(), name="issue-close"),
+    path("issues/<int:issue_number>/reopen/", IssueReOpenView.as_view(), name="issue-reopen"),
+    path("issues/<int:issue_number>/comments/", IssueCommentCreateView.as_view(), name="issue-comment-create"),
+    path("pull-requests/compare/", PullRequestCompareView.as_view(), name="pull-request-compare"),
+    path("pull-requests/", PullRequestListView.as_view(), name="pull-request-list"),
+    path("pull-requests/create/", PullRequestCreateView.as_view(), name="pull-request-create"),
 ]
 
 
@@ -52,18 +55,9 @@ urlpatterns = [
     path("accounts/register/", RegisterView.as_view(), name="accounts-register"),
     path("accounts/logout/", LogoutView.as_view(), name="accounts-logout"),
     path("create/", ProjectCreateView.as_view(), name="project-create"),
-    
-    path("<str:username>/<str:project_handle>/", ProjectOverview.as_view(), name="project-overview"),
-    path("<str:username>/<str:project_handle>/browse/", include(project_browse_urlpatterns)),
-    path("<str:username>/<str:project_handle>/issues/", IssueListView.as_view(), name="issue-list"),
-    path("<str:username>/<str:project_handle>/issues/create/", IssueCreateView.as_view(), name="issue-create"),
-    path("<str:username>/<str:project_handle>/issues/<int:issue_number>/", IssueDetailView.as_view(), name="issue-detail"),
-    path("<str:username>/<str:project_handle>/issues/<int:issue_number>/close/", IssueCloseView.as_view(), name="issue-close"),
-    path("<str:username>/<str:project_handle>/issues/<int:issue_number>/reopen/", IssueReOpenView.as_view(), name="issue-reopen"),
-    path("<str:username>/<str:project_handle>/issues/<int:issue_number>/comments/", IssueCommentCreateView.as_view(), name="issue-comment-create"),
-    path("<str:username>/<str:project_handle>/pull-requests/compare/", PullRequestCompareView.as_view(), name="pull-request-compare"),
-    path("<str:username>/<str:project_handle>/pull-requests/", PullRequestListView.as_view(), name="pull-request-list"),
-    path("<str:username>/<str:project_handle>/pull-requests/create/", PullRequestCreateView.as_view(), name="pull-request-create"),
+
+    # Project endpoints
+    path("<str:username>/<str:project_handle>/", include(project_urlpatterns)),
 
     # Git endpoints for handling git operations
     path("<str:username>/<str:project_handle>.git/info/refs", GitInfoRefsView.as_view(), name="project-git-info-refs"),
