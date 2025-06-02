d2b684c1c64d465a67c710bbae492a2e8ac09bac
Santosh Kumar Patro <santoshkpatro11@gmail.com>
2025-06-03 03:53:11 +0530

    feat: implement avatar upload functionality with Uppy integration and update profile management

--- gitsap/accounts/models.py
+++ gitsap/accounts/models.py
@@ -83,6 +83,8 @@ class User(BaseUUIDModel, AbstractBaseUser):
 
     @property
     def avatar_url(self):
+        if self.avatar:
+            return self.avatar.url
         email = self.email.strip().lower().encode("utf-8")
         email_hash = hashlib.md5(email).hexdigest()
         return f"https://www.gravatar.com/avatar/{email_hash}?s=64&d=retro"
